6f9aa5ae60ad0804cae2d8d4206924947e95369e
Jeremias <JeremiasOviedo.Dev@gmail.com>
2023-02-08 09:26:20 -0300

    SP1-4: DonationEntity created (#3)
    Co-authored-by: matiaseq <matiaseq@gmail.com>

--- Backend/src/main/java/com/nocountry/c930/entity/DonationEntity.java
+++ Backend/src/main/java/com/nocountry/c930/entity/DonationEntity.java
@@ -1,4 +1,40 @@
 package com.nocountry.c930.entity;
 
-public class DonationEntity {
+import com.nocountry.c930.enumeration.PaymentStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "DONATIONS")
+public class DonationEntity implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DONATION_ID")
+    private Long donationId;
+
+    @Column(name = "CREATION_DATE", nullable = false)
+    private Date creationDate;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "STATUS", nullable = false)
+    private PaymentStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_CAMPAIGN_ID")
+    private UserCampaign userCampaign;
+
+
+
 }
