7e5316727460847dfb83aaa850b976325a659389
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2023-02-10 02:46:09 -0300

    Refactored minor changes

--- Backend/src/main/java/com/nocountry/c930/entity/PaymentEntity.java
+++ Backend/src/main/java/com/nocountry/c930/entity/PaymentEntity.java
@@ -2,11 +2,7 @@ package com.nocountry.c930.entity;
 
 import javax.persistence.*;
 
-@Entity
 public class PaymentEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "PAYMENT_ID")
+    
     private String paymentId;
 }
