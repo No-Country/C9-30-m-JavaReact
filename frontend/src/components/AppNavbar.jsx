c657f812e5d84fb262765a1fa341f1b67ef5462c
LuisacnyBaudin <luisacbaudin@gmail.com>
2023-03-01 18:31:11 -0500

    Edit navbar

--- frontend/src/components/AppNavbar.jsx
+++ frontend/src/components/AppNavbar.jsx
@@ -1,17 +1,28 @@
-import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+ 
+} from "react-bootstrap";
+import { useState} from "react";
 import Logo from "../assets/logo.svg";
 import { useAuth } from "../context/AuthContext";
+import { BsSearch } from "react-icons/Bs";
 
 const token = localStorage.getItem("token");
 
 const AppNavbar = () => {
   const { logout } = useAuth();
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleLogout = () => {
     logout();
     window.location.reload();
   };
+
+
 
   return (
     <Navbar bg="light" expand="lg">
@@ -24,67 +35,80 @@ const AppNavbar = () => {
           <Nav className="justify-content-around" style={{ width: "60%" }}>
             {token === null ? (
               <>
-                <Nav.Link style={{ fontSize: "smaller" }} href="/login">
+                <Nav.Link style={{ fontSize: "15px" }} href="/login">
                   Login
                 </Nav.Link>
-                <Nav.Link style={{ fontSize: "smaller" }} href="/registro">
+                <Nav.Link style={{ fontSize: "15px" }} href="/registro">
                   Registro
                 </Nav.Link>
               </>
             ) : (
               <>
-                <div>
+                <div className="d-flex justify-content-around">
                   <form
                     className="d-flex"
-                    style={{ fontSize: "smaller", width: "80%" }}
+                    style={{
+                      borderRadius: "20px",
+                      width: "100%",
+                      height: "95%",
+                    }}
                     role="search"
+                     
                   >
                     <input
-                      className="form-control me-2"
+                      className="inputOne form-control text-center mt-1"
                       type="search"
-                      placeholder="buscador"
+                      placeholder="Buscador"
+                 
                     />
-                    <Nav.Link href="/buscar"></Nav.Link>
+                    <Nav.Link href="/buscar" className="buttonSearch d-inline">
+                      <BsSearch />
+                      <div>
+                        {searchResults.map((result) => (
+                          <li key={result.id}>{result.shortDescription}</li>
+                        ))}
+                      </div>
+                    </Nav.Link>
                   </form>
                 </div>
-                <Nav.Link style={{ fontSize: "smaller" }} href="/favorites">
+                <Nav.Link style={{ fontSize: "medium" }} href="/favorites">
                   Favoritos
                 </Nav.Link>
-                <Nav.Link style={{ fontSize: "smaller" }} href="/mensajes">
+                <Nav.Link style={{ fontSize: "medium" }} href="/mensajes">
                   Mensajes
                 </Nav.Link>
                 <NavDropdown
-                  style={{ fontSize: "smaller" }}
+                  style={{ fontSize: "medium" }}
                   title="Perfil"
                   id="basic-nav-dropdown"
                 >
                   <NavDropdown.Item
-                    style={{ fontSize: "smaller" }}
+                    style={{ fontSize: "medium" }}
                     href="#action/3.1"
                   >
                     Campañas creadas
                   </NavDropdown.Item>
                   <NavDropdown.Item
-                    style={{ fontSize: "smaller" }}
+                    style={{ fontSize: "medium" }}
                     href="#action/3.2"
                   >
                     Mis patrocinios
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item
-                    style={{ fontSize: "smaller" }}
+                    style={{ fontSize: "medium" }}
                     href="#action/3.3"
                   >
                     Mensajes
                   </NavDropdown.Item>
                   <NavDropdown.Item
-                    style={{ fontSize: "smaller" }}
+                    style={{ fontSize: "medium" }}
                     href="#action/3.4"
                   >
                     Configuraciones
                   </NavDropdown.Item>
                   <NavDropdown.Item
-                    style={{ fontSize: "smaller" }}
+                    style={{ fontSize: "medium" }}
                     href="#"
                     onClick={handleLogout}
                   >
@@ -92,7 +116,14 @@ const AppNavbar = () => {
                   </NavDropdown.Item>
                 </NavDropdown>
                 <a href="/add">
-                  <Button variant="primary" style={{ borderRadius: "20px" }}>
+                  <Button
+                    variant="primary"
+                    style={{
+                      borderRadius: "20px",
+                      width: "120%",
+                      height: "95%",
+                    }}
+                  >
                     Cargar proyecto
                   </Button>
                 </a>
