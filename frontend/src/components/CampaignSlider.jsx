3115c2c5e2f3da6e5dc5a4d6e6656db627bd51ea
Ignacio Garcia <nachogarcia003@gmail.com>
2023-03-02 14:20:32 -0300

    autoplay embla

--- frontend/src/components/CampaignSlider.jsx
+++ frontend/src/components/CampaignSlider.jsx
@@ -1,10 +1,10 @@
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useState } from "react";
-import { Card, ProgressBar } from "react-bootstrap";
-
+import { Card, ProgressBar, Spinner } from "react-bootstrap";
+import Autoplay from "embla-carousel-autoplay";
 import { searchCampaignsByKey } from "../js/search";
 
-const options = { dragFree: true, containScroll: "trimSnaps" };
+const options = { dragFree: true, containScroll: "trimSnaps", loop: false };
 
 const CampaignSlider = ({ data }) => {
   const [slides, setSlides] = useState([]);
@@ -18,40 +18,44 @@ const CampaignSlider = ({ data }) => {
     run();
   }, []);
 
-  const [emblaRef] = useEmblaCarousel(options);
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay()]);
   return (
     <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((e, idx) => {
-            return (
-              <div className="embla__slide" key={idx}>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src={e.bannerUrl}
-                    width={286}
-                    height={162}
-                  />
-                  <ProgressBar
-                    className="campaign-progress"
-                    now={(e.currentMoney * 100) / e.goalMoney}
-                  />
-                  <Card.Body>
-                    <Card.Title>{e.name}</Card.Title>
-                    <Card.Text>{e.shortDescription}</Card.Text>
-                    <p>
-                      ${e.currentMoney} / {e.goalMoney}
-                    </p>
+      {slides.length ? (
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((e, idx) => {
+              return (
+                <div className="embla__slide" key={idx}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img
+                      variant="top"
+                      src={e.bannerUrl}
+                      width={286}
+                      height={162}
+                    />
+                    <ProgressBar
+                      className="campaign-progress"
+                      now={(e.currentMoney * 100) / e.goalMoney}
+                    />
+                    <Card.Body>
+                      <Card.Title>{e.name}</Card.Title>
+                      <Card.Text>{e.shortDescription}</Card.Text>
+                      <p>
+                        ${e.currentMoney} / {e.goalMoney}
+                      </p>
 
-                    <a href={`/campania/${e.campaignId}`}>ver mas</a>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })}
+                      <a href={`/campania/${e.campaignId}`}>ver mas</a>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <Spinner animation="border" />
+      )}
     </div>
   );
 };
